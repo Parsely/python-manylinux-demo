24a935bec692403f31eec2d50dff50ef6eebd06a
Dan Blanchard <dan.blanchard@gmail.com>
2020-03-17 14:31:17 -0400

    Set up wheel builder for travis

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -2,21 +2,51 @@
 set -e -x
 
 # Install a system package required by our library
-yum install -y atlas-devel
+yum install -y zlib-devel bzip2-devel ncurses-devel sqlite-devel \
+readline-devel tk-devel gdbm-devel db4-devel libpcap-devel\
+xz-devel gpg atlas-devel libev-devel libev snappy-devel
+python-imaging openjpeg-devel freetype-devel libpng-devel \
+libffi-devel python-lxml postgresql95-libs \
+postgresql95-devel lapack-devel python \
+python-devel python-setuptools pcre pcre-devel \
+pandoc
+
+# psycopg2 won't build without this
+export PG_HOME=/usr/pgsql-9.5
+export PATH=/usr/pgsql-9.5/bin:$PATH
 
 # Compile wheels
 for PYBIN in /opt/python/*/bin; do
-    "${PYBIN}/pip" install -r /io/dev-requirements.txt
-    "${PYBIN}/pip" wheel /io/ -w wheelhouse/
+    if [[ "$PYBIN" =~ cp26 ]]; then
+        echo "Skipping 2.6 because it's horrible"
+        elif [[ "$PYBIN" =~ cp27 ]]; then
+        echo "Skipping 2.7 because we don't use it"
+        elif [[ "$PYBIN" =~ cp33 ]]; then
+        echo "Skipping 3.3 because we don't use it"
+        elif [[ "$PYBIN" =~ cp34 ]]; then
+        echo "Skipping 3.4 because we don't use it"
+        elif [[ "$PYBIN" =~ cp35 ]]; then
+        echo "Skipping 3.5 because we don't use it"
+    else
+        CFLAGS="-I/usr/local/ssl/include" LDFLAGS="-L/usr/local/ssl/lib" ${PYBIN}/pip wheel cryptography -w wheelhouse/ -f wheelhouse
+        "${PYBIN}/pip" wheel -r /io/dev-requirements.txt -w wheelhouse/ -f wheelhouse || true
+        # Do another run allowing dev builds, and do it with a separate run per
+        # requirement so that one broken prerelease doesn't stop the rest from
+        # being build---I'm looking at *you* statsmodel 0.8.0rc1
+        cat /io/dev-requirements.txt | tr '\n' '\0' | xargs -0 -I{} bash -c "${PYBIN}/pip wheel --pre {} -w wheelhouse/ -f wheelhouse || true"
+    fi
 done
 
 # Bundle external shared libraries into the wheels
 for whl in wheelhouse/*.whl; do
-    auditwheel repair "$whl" --plat $PLAT -w /io/wheelhouse/
+    if [[ "$whl" =~ none-any ]]; then
+        echo "Skipping pure wheel $whl"
+        elif [[ "$whl" =~ manylinux ]]; then
+        echo "Skipping manylinux wheel $whl"
+    else
+        auditwheel repair "$whl" --plat $PLAT -w /io/wheelhouse/ || true
+    fi
 done
 
-# Install packages and test
-for PYBIN in /opt/python/*/bin/; do
-    "${PYBIN}/pip" install python-manylinux-demo --no-index -f /io/wheelhouse
-    (cd "$HOME"; "${PYBIN}/nosetests" pymanylinuxdemo)
-done
+# Remove platform-specific wheels
+rm -f wheelhouse/*-linux*.whl
